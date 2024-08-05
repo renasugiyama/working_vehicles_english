276f19d938a239416b37ae8a925baf6e3b34b08b
Rena Sugiyama <alina172021@gmail.com>
2024-08-05 14:21:49 +0900

    Update Babel config and dependencies

--- babel.config.js
+++ babel.config.js
@@ -54,17 +54,18 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-proposal-private-methods',
+        '@babel/plugin-transform-private-methods',
         {
           loose: true
         }
       ],
       [
-        '@babel/plugin-proposal-private-property-in-object',
+        '@babel/plugin-transform-private-property-in-object',
         {
           loose: true
         }
       ],
+      '@babel/plugin-transform-optional-chaining',
       [
         '@babel/plugin-transform-runtime',
         {
