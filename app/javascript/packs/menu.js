de6295d2b24d91ed8a775486a632367cebea2100
Rena Sugiyama <alina172021@gmail.com>
2024-08-05 14:41:04 +0900

    Update menu.js

--- app/javascript/packs/menu.js
+++ app/javascript/packs/menu.js
@@ -2,9 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var menuButton = document.getElementById("menuButton");
   var menuDropdown = document.getElementById("menuDropdown");
 
-  if (menuButton && menuDropdown) {
-    menuButton.addEventListener("click", function() {
-      menuDropdown.classList.toggle("hidden");
-    });
-  }
+  menuButton.addEventListener("click", function() {
+    menuDropdown.classList.toggle("hidden");
+  });
 });
