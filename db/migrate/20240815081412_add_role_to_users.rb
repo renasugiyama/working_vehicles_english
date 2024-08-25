3e4a1a319b5c3c0ea7f6b79b4042834c03f150fa
Rena Sugiyama <alina172021@gmail.com>
2024-08-25 15:31:18 +0900

    Fixed Gemfile and resolved migration issue

--- db/migrate/20240815081412_add_role_to_users.rb
+++ db/migrate/20240815081412_add_role_to_users.rb
@@ -1,5 +1,7 @@
 class AddRoleToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :role, :integer, default: 0, null: false
+    unless column_exists?(:users, :role)
+      add_column :users, :role, :integer, default: 0, null: false
+    end
   end
 end
