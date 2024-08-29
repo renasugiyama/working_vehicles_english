a984364f9fb99f0ac4e37d491aead2a338ccde99
Rena Sugiyama <alina172021@gmail.com>
2024-08-29 14:05:15 +0900

    メールアドレスの変更機能を追加

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,4 +10,10 @@ class UserMailer < ApplicationMailer
     @url  = edit_password_reset_url(@user.reset_password_token)
     mail(to: @user.email, subject: "パスワードリセットのお知らせ")
   end
+
+  def email_change_confirmation(user)
+    @user = user
+    @url = confirm_email_change_user_url(@user, token: @user.confirmation_token)
+    mail(to: @user.unconfirmed_email, subject: "メールアドレス確認のお願い")
+  end
 end
