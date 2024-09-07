581a5ea04bf5e2a0dddf3ff05398099df962feae
Rena Sugiyama <alina172021@gmail.com>
2024-09-07 19:08:07 +0900

    Update production configuration for asset compilation

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -12,4 +12,24 @@ class RewardsController < ApplicationController
       }
     end
   end
+
+  # 全プレイヤーを表示するアクション
+  def players
+    @players = current_user.players # ログインしているユーザーの全プレイヤーを取得
+  end
+
+  # 特定のプレイヤーのRewardを表示するアクション
+  def player_rewards
+    @player = current_user.players.find(params[:id]) # URLのIDに基づいてプレイヤーを取得
+    @rewards = Reward.all.map do |reward|
+      achieved = @player.rewards.include?(reward)
+      # 達成状況に応じてアイコンパスを切り替え
+      icon_path = achieved ? "#{reward.icon}_color.png" : "#{reward.icon}_gray.png"
+      {
+        reward: reward,
+        achieved: achieved,
+        icon: icon_path
+      }
+    end
+  end
 end
